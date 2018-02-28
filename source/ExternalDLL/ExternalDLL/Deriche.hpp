15eb01a2d4571a5af10933f8add04b4a75e7e509
Unknown <stein.bout@xs4all.nl>
2018-02-28 16:56:54 +0100

    WIP effecient array

--- source/ExternalDLL/ExternalDLL/Deriche.hpp
+++ source/ExternalDLL/ExternalDLL/Deriche.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <math.h>
+#include <array>
 #include <opencv2/imgproc/imgproc.hpp>
 
 
@@ -23,24 +24,32 @@ public:
 		b2 = -1 * expNeq2Sigma;
 	}
 
-	void smooth(const cv::Mat & imageMatrix) {
+	void smooth(cv::Mat & imageMatrix) {
+
+		//asserting if its not CV_8U (unsigned char type matrix)
+		CV_Assert(imageMatrix.depth() == CV_8U);
+
 		// setting up smoothing variables
 		a1 = k;
 		a2 = k * expNeqSigma * (sigma - 1);
 		a3 = k * expNeqSigma * (sigma + 1);
 		a4 = -k * expNeq2Sigma;
 
-		for (size_t m = 0; m < imageMatrix.cols; ++m) {
-			for (size_t n = 2; n < imageMatrix.rows; ++n) {
+		size_t rows = imageMatrix.rows;
+		size_t cols = imageMatrix.cols;
+		size_t i, j;
+		uchar* pixelPointer;
+		std::array<uchar,cols> y1;
+		uchar y2[cols];
 
-			}
-			for (size_t n = imageMatrix.rows - 3; n >= 0; --n) {
+		for (i = 0; i < rows; ++i) {
+			pixelPointer = imageMatrix.ptr<uchar>(i);
 
-			}
-			for (size_t n = 0; n < imageMatrix.rows; ++n) {
+			//do stuff for first 2 pixels
+
+			for (j = 2; j < cols; ++j) {
 
 			}
 		}
-
 	}
 };
