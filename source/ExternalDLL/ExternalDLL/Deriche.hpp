77acc8a187fe7ccb74d685ff005b347ad8c64d26
Unknown <stein.bout@xs4all.nl>
2018-03-01 16:02:06 +0100

    Bugfix

--- source/ExternalDLL/ExternalDLL/Deriche.hpp
+++ source/ExternalDLL/ExternalDLL/Deriche.hpp
@@ -69,7 +69,7 @@ public:
 
 			//do other stuff for first 2 pixels..
 			y2[cols-1] = a3 * pixelPointer[cols-1];
-			y2[cols-2] = a3 * pixelPointer[cols-2] + a4 * pixelPointer[0] + b1 * y2[cols - 1];
+			y2[cols-2] = a3 * pixelPointer[cols-2] + a4 * pixelPointer[cols - 2] + b1 * y2[cols - 1];
 
 			for (j = cols - 3; j >= 0; --j) {
 				y2[j] = a3 * pixelPointer[j+1] + a4 * pixelPointer[j + 2] + b1 * y2[j + 1] + b2 * y2[j + 2];
@@ -84,10 +84,10 @@ public:
 		}
 
 		for (j = 0; j < cols; ++j) {
-			pixelPointer = imageMatrix.ptr<uchar>(j);
+			pixelPointer = tempMatrix.ptr<uchar>(j);
 
 			//do stuff for first 2 pixels NOTE: might need improvement.
-			y1[0] = a1 * pixelPointer[0];
+			y1[0] = a1 * pixelPointer[0] + a2 * pixelPointer[0];
 			y1[1] = a1 * pixelPointer[1] + a2 * pixelPointer[0] + b1 * y1[0];
 
 
@@ -97,7 +97,7 @@ public:
 
 			//do other stuff for first 2 pixels..
 			y2[rows - 1] = a3 * pixelPointer[rows - 1];
-			y2[rows - 2] = a3 * pixelPointer[rows - 2] + a4 * pixelPointer[0] + b1 * y2[rows - 1];
+			y2[rows - 2] = a3 * pixelPointer[rows - 2] + a4 * pixelPointer[rows - 2] + b1 * y2[rows - 1];
 
 			for (i = rows - 3; i >= 0; --i) {
 				y2[i] = a3 * pixelPointer[i + 1] + a4 * pixelPointer[i + 2] + b1 * y2[i + 1] + b2 * y2[i + 2];
@@ -107,7 +107,7 @@ public:
 			for (i = 0; i < rows; ++i) {
 
 				//we transpose the image back here!
-				outputMatrix.at<uchar>(j, i) = (uchar)(y1[i] + y2[i]);
+				outputMatrix.at<uchar>(i, j) = (uchar)(y1[i] + y2[i]);
 			}
 		}
 
