f39cb09081aa873bb53c2902f361fdbdad870f7e
Stieno <stein.bout@xs4all.nl>
2018-03-09 14:35:17 +0100

    Fixed black image as output

--- source/ExternalDLL/ExternalDLL/Deriche.hpp
+++ source/ExternalDLL/ExternalDLL/Deriche.hpp
@@ -69,7 +69,7 @@ private:
 			for (j = 0; j < cols; ++j) {
 
 				//we transpose the image here!
-				dst.at<uchar>(j, i) = (uchar)(y1[j] + y2[j]);
+				dst.at<uchar>(j, i) = c * (uchar)(y1[j] + y2[j]);
 			}
 		}
 	}
@@ -91,7 +91,7 @@ public:
 		a2 = k * expNeqSigma * (sigma - 1);
 		a3 = k * expNeqSigma * (sigma + 1);
 		a4 = -k * expNeq2Sigma;
-		c = -pow((1 - expNeqSigma), 2.0);
+		c = -pow(1 - expNeqSigma, 2.0);
 
 		std::cout << "DEBUG\n" <<
 			"Sigma: " << sigma << "\n " <<
@@ -134,10 +134,12 @@ public:
 
 		dericheIIR2(matrix, tempMatrix, rows, cols, y1, y2);
 		dericheIIR(tempMatrix, matrix, cols, rows, y1, y2);
-		matrix *= c;
-		dericheIIR(matrix, tempMatrix, cols, rows, y1, y2);
-		tempMatrix *= c;
-		dericheIIR2(tempMatrix, matrix, rows, cols, y1, y2);
+		dericheIIR(matrix, tempMatrix, rows, cols, y1, y2);
+		dericheIIR2(tempMatrix, matrix, cols, rows, y1, y2);
+
+
+		
+
 	}
 
 };
